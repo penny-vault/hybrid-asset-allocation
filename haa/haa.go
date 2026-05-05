ec88524b1a9dbee0663c89e7a81daeef1f41fca8
Jeremy Fergason <jeremy@fergason.me>
2026-05-04 23:12:07 -0700

    chore: release v0.2.0

--- haa/haa.go
+++ haa/haa.go
@@ -56,7 +56,7 @@ func (s *HybridAssetAllocation) Describe() engine.StrategyDescription {
 		Version:     "1.0.0",
 		VersionDate: time.Date(2026, 3, 14, 0, 0, 0, 0, time.UTC),
 		Schedule:    "@monthend",
-		Benchmark:   "VFINX",
+		Benchmark:   "SPY",
 	}
 }
 
